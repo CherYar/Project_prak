21626ccfc48a2c17d4c2743497a8baa3e162c3da
CherYar <yarslvchern@gmail.com>
2023-05-29 23:40:33 +0300

    ОНО РАБОТАЕТ! Фиксация перед хитроумными изменениями, которые могут всё запороть.

--- Project.cpp
+++ Project.cpp
@@ -2,28 +2,17 @@
 #include <fstream>
 #include <windows.h>
 #include <string>
-#include <time.h>
 #include <iomanip>
 #include <cctype>
 #include <limits>
 #include <functional>
 #include <algorithm>
-#include <openssl/rand.h>
-#include <openssl/evp.h>
+#include <openssl/rand.h>//Для создания случайного ключа
+#include <openssl/evp.h>//Для использования стандарта шифрования AES
+using namespace std;
+#define CinDel cin.ignore((numeric_limits<streamsize>::max)(), '\n');//Очистка буфера ввода
 
-#define BUF_SIZE 1024
-#define CinDel cin.ignore((numeric_limits<streamsize>::max)(), '\n');
-
-bool ValidInt(string num) {
-    if (num == "" || num.length() > 9)
-        return 0;
-    num.append("a");
-    if (num.find_first_not_of("0123456789+-") != num.length() - 1)
-        return 0;
-    return 1;
-}
-
-bool ValidIntModernised(const string& num) {
+bool ValidIntModernised(const string& num) {//Проверки на возможность корректного перевода строки в число
     if (num.empty() || num.size() > 9) {
         return false;
     }
@@ -40,15 +29,6 @@ bool ValidIntModernised(const string& num) {
         }
     }
     return true;
-}
-
-bool ValidFloat(string num) {
-    if (num == "" || num.length() > 9)
-        return 0;
-    num.append("a");
-    if (num.find_first_not_of("0123456789.-+") != num.length() - 1)
-        return 0;
-    return 1;
 }
 
 bool ValidFloatModernised(const string& num) {
@@ -73,7 +53,7 @@ bool ValidFloatModernised(const string& num) {
     }
     return true;
 }
-int readIntV(const string& prompt, const function<bool(int)>& validator) {
+int readIntV(const string& prompt, const function<bool(int)>& validator) {//Функции для чтения с клавиатуры в переменную с проверкой на корректность
     string input;
     cout << prompt;
     while (cin >> input) {
@@ -85,7 +65,6 @@ int readIntV(const string& prompt, const function<bool(int)>& validator) {
         }
         cout << "Некорректное значение. Пожалуйста, введите заново: ";
     }
-    CinDel
         return 0;
 }
 int readIntLV(const string& prompt, const function<bool(long)>& validator) {
@@ -100,25 +79,9 @@ int readIntLV(const string& prompt, const function<bool(long)>& validator) {
         }
         cout << "Некорректное значение. Пожалуйста, введите заново: ";
     }
-    CinDel
         return 0;
 }
 
-int readIntV(const string& prompt, const function<bool(int)>& validator, const int& defaultValue) {
-    string input;
-    cout << prompt;
-    while (cin >> input) {
-        if (ValidIntModernised(input)) {
-            int value = stoi(input);
-            if (validator(value)) {
-                return value;
-            }
-        }
-        cout << "Некорректное значение. Пожалуйста, введите заново: ";
-    }
-    CinDel
-        return defaultValue;
-}
 int readInt(const string& prompt) {
     string input;
     cout << prompt;
@@ -129,7 +92,6 @@ int readInt(const string& prompt) {
         }
         cout << "Некорректное значение. Пожалуйста, введите заново: ";
     }
-    CinDel
         return 0;
 }
 string readStr(const string& prompt) {
@@ -140,23 +102,9 @@ string readStr(const string& prompt) {
             return input;
         }
     }
-    CinDel
 }
 
-string readStrV(const string& prompt, const function<bool(string)>& validator) {
-    string input;
-    cout << prompt;
-    while (getline(cin, input)) {
-        if (!input.empty()) {
-            if (validator(input)) {
-                return input;
-            }
-        }
-        cout << "Некорректное значение. Пожалуйста, введите заново: ";
-    }
-    CinDel
-}
-string readStrW(const string& prompt, const function<bool(const string&)>& validator) {
+string readStrV(const string& prompt, const function<bool(const string&)>& validator) {
     string input;
     while (true) {
         cout << prompt;
@@ -166,169 +114,206 @@ string readStrW(const string& prompt, const function<bool(const string&)>& valid
         }
         cout << "Некорректное значение. Пожалуйста, введите заново." << endl;
     }
-    CinDel
 }
-using namespace std;
-bool generate_random_key(unsigned char* key, int keylen) {
+
+bool mrandkey(unsigned char* key, int keylen) {//Создание криптографиечески стойкого случайного ключа, используя генератор случайных чисел библиотеки OpenSSL. Обычные средства генерации рандомных чисел в C++ могуть быть предсказуемыми. Хотя для такого простого проекта это не так уж и важно
     if (!RAND_bytes(key, keylen)) {
         return false;
     }
     return true;
 }
-/*int menu() {//Главное меню
-    Decrypt();
-    list.ListFromFileBin("database.bin");
-    cout << "База данных расшифрована. Программа готова к работе." << endl; system("pause");
-    int mchoice = -1;
-    for (; mchoice != 0;)
+int encrypt(const string& filename, const unsigned char* key, const unsigned char* iv) {//Шифрование файла
+    // Открытие входного файла
+    ifstream input(filename, ios::binary);
+    if (!input.is_open()) {
+        cerr << "Ошибка при открытии файла " << filename << endl;
+        return 1;
+    }
+
+    // Создание выходного файла
+    ofstream output(filename + ".enc", ios::binary);
+    if (!output.is_open()) {
+        cerr << "Ошибка при создании выходного файла" << endl;
+        input.close();
+        return 1;
+    }
+
+    // Создание и инициализация контекста шифрования
+    EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
+    if (!ctx) {
+        cerr << "Ошибка при создании контекста шифрования" << endl;
+        input.close();
+        output.close();
+        return 1;
+    }
+    if (!EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), nullptr, key, iv)) {
+        cerr << "Ошибка при инициализации контекста шифрования" << endl;
+        EVP_CIPHER_CTX_free(ctx);
+        input.close();
+        output.close();
+        return 1;
+    }
+
+    // Чтение данных из входного файла и шифрование их
+    const size_t buffer_size = 4096;
+    unsigned char input_buffer[buffer_size];
+    unsigned char output_buffer[buffer_size + EVP_MAX_BLOCK_LENGTH];
+    int output_len = 0;
+    while (input.good()) {
+        input.read(reinterpret_cast<char*>(input_buffer), buffer_size);
+        size_t input_len = input.gcount();
+        if (!EVP_EncryptUpdate(ctx, output_buffer, &output_len, input_buffer, input_len)) {
+            cerr << "Ошибка при шифровании данных" << endl;
+            EVP_CIPHER_CTX_free(ctx);
+            input.close();
+            output.close();
+            return 1;
+        }
+        output.write(reinterpret_cast<char*>(output_buffer), output_len);
+    }
+
+    // Завершение шифрования
+    if (!EVP_EncryptFinal_ex(ctx, output_buffer, &output_len)) {
+        cerr << "Ошибка при завершении шифрования" << endl;
+        EVP_CIPHER_CTX_free(ctx);
+        input.close();
+        output.close();
+        return 1;
+    }
+    output.write(reinterpret_cast<char*>(output_buffer), output_len);
+
+    // Освобождение ресурсов
+    EVP_CIPHER_CTX_free(ctx);
+    input.close();
+    output.close();
+
+    cout << "Файл " + filename + " успешно зашифрован" << endl;
+    return 0;
+}
+
+int decrypt(const string& filename, const unsigned char* key, const unsigned char* iv) {//Дешифрование файла
+    // Открытие входного файла
+    ifstream input(filename, ios::binary);
+    if (!input.is_open()) {
+        cerr << "Ошибка при открытии файла " << filename << endl;
+        return 1;
+    }
+
+    // Создание выходного файла
+    ofstream output(filename + ".dec", ios::binary);
+    if (!output.is_open()) {
+        cerr << "Ошибка при создании выходного файла" << endl;
+        input.close();
+        return 1;
+    }
+
+    // Создание и инициализация контекста расшифровки
+    EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
+    if (!ctx) {
+        cerr << "Ошибка при создании контекста расшифровки" << endl;
+        input.close();
+        output.close();
+        return 1;
+    }
+    if (!EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), nullptr, key, iv)) {
+        cerr << "Ошибка при инициализации контекста расшифровки" << endl;
+        EVP_CIPHER_CTX_free(ctx);
+        input.close();
+        output.close();
+        return 1;
+    }
+
+    // Чтение данных из входного файла и расшифровка их
+    const size_t buffer_size = 4096;
+    unsigned char input_buffer[buffer_size];
+    unsigned char output_buffer[buffer_size + EVP_MAX_BLOCK_LENGTH];
+    int output_len = 0;
+    while (input.good()) {
+        input.read(reinterpret_cast<char*>(input_buffer), buffer_size);
+        size_t input_len = input.gcount();
+        if (!EVP_DecryptUpdate(ctx, output_buffer, &output_len, input_buffer, input_len)) {
+            cerr << "Ошибка при расшифровке данных" << endl;
+            EVP_CIPHER_CTX_free(ctx);
+            input.close();
+            output.close();
+            return 1;
+        }
+        output.write(reinterpret_cast<char*>(output_buffer), output_len);
+    }
+
+    // Завершение расшифровки
+    if (!EVP_DecryptFinal_ex(ctx, output_buffer, &output_len)) {
+        cerr << "Ошибка при завершении расшифровки" << endl;
+        EVP_CIPHER_CTX_free(ctx);
+        input.close();
+        output.close();
+        return 1;
+    }
+    output.write(reinterpret_cast<char*>(output_buffer), output_len);
+
+    // Освобождение ресурсов
+    EVP_CIPHER_CTX_free(ctx);
+    input.close();
+    output.close();
+
+    cout << "Файл " + filename + " успешно расшифрован" << endl;
+    return 0;
+}
+
+int menu() {//Меню для удобной работы с программой
+    unsigned char key[32];
+    if (!mrandkey(key, sizeof(key))) {
+        cerr << "Ошибка при генерации случайного ключа" << endl;
+        return 1;
+    }
+    unsigned char iv[16];
+    if (!mrandkey(iv, sizeof(iv))) {
+        cerr << "Ошибка при генерации вектора инициализации" << endl;
+        return 2;
+    }
+    int choice = -1;
+    cout << "Добро пожаловать в простую программу шифрования. Для продолжения нажмите ENTER." << endl;
+    for (; choice != 0;)
     {
         CinDel
-            unsigned short lsiz = list.getSize(); string lsize = to_string(lsiz);
         system("cls");
-        cout << "Текущее количество студентов в базе данных: " + lsize << '\n';
         cout << "-------------------------------------------------------------\n";
         cout << "Выберете действие:\n";
         cout << "-------------------------------------------------------------\n";
-        cout << "1.Вывод краткой информации о всех студентах\n";
-        cout << "2.Вывод полной информации о всех студентах (не рекомендуется)\n";
-        cout << "3.Вывод полной информации о конкретном студенте\n";
+        cout << "1.Зашифровать файл\n";
+        cout << "2.Расшифровать файл\n";
         cout << "-------------------------------------------------------------\n";
-        cout << "4.Изменение данных конкретного студента\n";
-        cout << "5.Добавление нового студента с вводом с клавиатуры\n";
-        cout << "6.Удаление студента из базы данных\n";
+        cout << "3.Показать информацию о программе\n";
         cout << "-------------------------------------------------------------\n";
-        cout << "7.Выполнить задание варианта 69 (разбиение и сортировка)\n";
-        cout << "-------------------------------------------------------------\n";
-        cout << "0.Выход из программы, с последующим шифрованием базы данных\n";
+        cout << "0.Выход из программы\n";
         cout << "-------------------------------------------------------------\n";
         cout << ">";
-        mchoice = readIntV(" ", [](int c) {return c >= 0 && c <= 7; });
-        switch (mchoice)
+        choice = readIntV(" ", [](int c) {return c >= 0 && c <= 3; });
+        switch (choice)
         {
-        case 1: {cout << endl; list.printAll(); system("pause"); break; }
-        case 2: {cout << endl; list.printMNOGO(); system("pause"); break; }
-        case 3: {CinDel cout << endl; string prompt = "Введите номер студента(1 - " + lsize + "): ";
-            unsigned short index = readIntV(prompt, [&](int i) { return i >= 1 && i <= (lsiz); });
-            list.printStudent(index - 1); system("pause"); break; }
-        case 4: {CinDel cout << endl; string prompt = "Введите номер студента(1 - " + lsize + "): ";
-            unsigned short index = readIntV(prompt, [&](int i) { return i >= 1 && i <= (lsiz); }); list.studsubmenu(index - 1); system("pause"); break; }
-        case 5: {if (lsiz >= 15) { cout << "\nНевозможно добавить студента, достигнут предел." << endl; system("pause"); break; } else { CinDel student nstud; nstud.makestudent(); list.addStudent(nstud); cout << "\nСтудент добавлен" << endl; system("pause"); break; }  }
-        case 6: { CinDel cout << endl; string prompt = "Введите номер студента(1 - " + lsize + "): ";
-            unsigned short index = readIntV(prompt, [&](int i) { return i >= 1 && i <= (lsiz); }); list.removeStudent(index - 1); cout << "\nСтудент удалён." << endl; system("pause"); break;  }
-        case 7: { CinDel cout << endl; unsigned int mx, mi; mi = readIntV("Введите минимальный год рождения для сортировки:", [](int y) { return y >= 1950 && y <= 2030; });
-            mx = readIntV("Введите максимальный год рождения для сортировки:", [](int y) { return y >= 1950 && y <= 2030; });
-            splitStudentsByGrades(list, mi, mx); system("pause"); break; }
-        case 0: { list.ListToFileBin("database.bin"); Crypt(); cout << "\nРабота программы завершена, база данных зашифрована."; return 0; system("pause"); }
+        case 0: {cout << "\nРабота программы завершена." << endl; break; return 0; }
+        case 1: {CinDel string filename = readStr("\nВведите имя файла для шифрования (или 0 для отмены) :"); if (filename == "0") { cout << "\nОтмена операции, возвращение в главное меню." << endl; system("pause"); break; }
+              else {
+            encrypt(filename, key, iv);
+            system("pause");
+            break;}}
+        case 2: {CinDel string filename = readStr("\nВведите имя файла для дешифрования (или 0 для отмены) :"); if (filename == "0") { cout << "\nОтмена операции, возвращение в главное меню." << endl; system("pause"); break; }
+              else {
+            decrypt(filename, key, iv);
+            system("pause");
+            break;
+        }}
+        case 3: {cout << "\nДанная программа написана на языке C++ стандарта С++20 в Microsoft Visual Studio 2022.\nОна предназначена для шифрования/расшифровывания файлов используя стандарт AES-256 из библиотеки OpenSSL v3.1.0." << endl; system("pause"); break; }
         default: {cout << "\nНеизвестная операция!\n"; CinDel break; }
         }
     }
     return 0;
-}*/
-
-int encrypt_file(const char* in_filename, const char* out_filename, const char* key, const char* iv) {
-    EVP_CIPHER_CTX* ctx;
-    const EVP_CIPHER* cipher;
-    unsigned char in_buf[BUF_SIZE], out_buf[BUF_SIZE + EVP_MAX_BLOCK_LENGTH];
-    int in_len, out_len, len;
-
-    // Set up the encryption context and algorithm
-    OpenSSL_add_all_algorithms();
-    cipher = EVP_get_cipherbyname("aes-256-cbc");
-    ctx = EVP_CIPHER_CTX_new();
-    EVP_CIPHER_CTX_init(ctx);
-    EVP_EncryptInit_ex(ctx, cipher, NULL, (unsigned char*)key, (unsigned char*)iv);
-
-    // Open the input and output files and encrypt the data
-    ifstream fin(in_filename, ios::binary);
-    if (!fin) {
-        cerr << "Unable to open input file.\n";
-        return 1;
-    }
-    ofstream fout(out_filename, ios::binary);
-    if (!fout) {
-        cerr << "Unable to open output file.\n";
-        return 1;
-    }
-    while (fin) {
-        in_len = fin.readsome((char*)in_buf, BUF_SIZE);
-        if (in_len > 0) {
-            if (!EVP_EncryptUpdate(ctx, out_buf, &out_len, in_buf, in_len)) {
-                cerr << "Error encrypting data.\n";
-                EVP_CIPHER_CTX_cleanup(ctx);
-                return 1;
-            }
-            fout.write((char*)out_buf, out_len);
-        }
-    }
-    if (!EVP_EncryptFinal_ex(ctx, out_buf, &out_len)) {
-        cerr << "Error finalizing encryption.\n";
-        EVP_CIPHER_CTX_cleanup(ctx);
-        return 1;
-    }
-    fout.write((char*)out_buf, out_len);
-
-    return 0;
 }
 
-int decrypt_file(const char* in_filename, const char* out_filename, const char* key, const char* iv) {
-    EVP_CIPHER_CTX* ctx;
-    const EVP_CIPHER* cipher;
-    unsigned char in_buf[BUF_SIZE + EVP_MAX_BLOCK_LENGTH], out_buf[BUF_SIZE];
-    int in_len, out_len, len;
 
-    // Set up the decryption context and algorithm
-    OpenSSL_add_all_algorithms();
-    cipher = EVP_get_cipherbyname("aes-256-cbc");
-    ctx = EVP_CIPHER_CTX_new();
-    EVP_CIPHER_CTX_init(ctx);
-    EVP_DecryptInit_ex(ctx, cipher, NULL, (unsigned char*)key, (unsigned char*)iv);
-
-    // Open the input and output files and decrypt the data
-    ifstream fin(in_filename, ios::binary);
-    if (!fin) {
-        cerr << "Unable to open input file.\n";
-        return 1;
-    }
-    ofstream fout(out_filename, ios::binary);
-    if (!fout) {
-        cerr << "Unable to open output file.\n";
-        return 1;
-    }
-    while (fin) {
-        in_len = fin.readsome((char*)in_buf, BUF_SIZE + EVP_MAX_BLOCK_LENGTH);
-        if (in_len > 0) {
-            if (!EVP_DecryptUpdate(ctx, out_buf, &out_len, in_buf, in_len)) {
-                cerr << "Error decrypting data.\n";
-                EVP_CIPHER_CTX_cleanup(ctx);
-                return 1;
-            }
-            fout.write((char*)out_buf, out_len);
-        }
-    }
-    if (!EVP_DecryptFinal_ex(ctx, out_buf, &out_len)) {
-        cerr << "Error finalizing decryption.\n";
-        EVP_CIPHER_CTX_cleanup(ctx);
-        return 1;
-    }
-    fout.write((char*)out_buf, out_len);
-
-    return 0;
-}
 
 int main() {
-    const char* in_filename = "input.txt";
-    const char* enc_filename = "encrypted.dat";
-    const char* dec_filename = "decrypted.txt";
-    const char* key = "01234567890123456789012345678901";
-    const char* iv = "1234123412341234";
-    // Decrypt the encrypted file
-    if (decrypt_file(enc_filename, dec_filename, key, iv) != 0) {
-        cerr << "Error decrypting file.\n";
-        return 1;
-    }
-
-    // Output results
-    cout << "Encryption and decryption completed successfully.\n";
-    return 0;
+    SetConsoleCP(1251); SetConsoleOutputCP(1251);//Установка корректной кодировки
+    menu();
 }
 
